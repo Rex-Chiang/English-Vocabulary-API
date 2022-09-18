d16199acf90e1e6b945be46df6105e365ed5da39
Rex-Chiang <Rex.Chiang@luxshare-ict.com>
2022-09-18 12:10:20 +0800

    [feat] Add Update and delete in view

--- vocabulary/views.py
+++ vocabulary/views.py
@@ -10,11 +10,16 @@ from vocabulary.serializers import VocabularySerializer
 logger = logging.getLogger(settings.LOG_VERSION)
 
 class VocabularyView(viewsets.ModelViewSet):
-    http_method_names = ["get", "post"]
+    http_method_names = ["get", "post", "delete"]
     serializer_class = VocabularySerializer
+    lookup_url_kwarg = "english_word"
 
     def get_queryset(self):
         max_id = Vocabulary.objects.aggregate(max_id=Max("id"))["max_id"]
+
+        if not max_id:
+            return {}
+
         while True:
             pk = random.randint(1, max_id)
             vocabulary = Vocabulary.objects.filter(pk=pk)
@@ -24,8 +29,19 @@ class VocabularyView(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         try:
-            if Vocabulary.objects.filter(english_word=request.data.get("english_word")).exists():
-                return Response({"message": "Already Exists"}, status=status.HTTP_409_CONFLICT)
+            english_word = request.data.get("english_word")
+            vocabulary = Vocabulary.objects.filter(english_word=english_word)
+
+            if vocabulary.exists():
+                logger.info(f"{english_word} is exist.")
+                serializer = VocabularySerializer(vocabulary.first(), data=request.data)
+
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+                else:
+                    logger.error(serializer.errors)
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
             serializer = VocabularySerializer(data=request.data)
 
@@ -39,4 +55,9 @@ class VocabularyView(viewsets.ModelViewSet):
         except Exception as exception_message:
             logger.exception(exception_message)
             return Response({"message": exception_message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def destroy(self, request, english_word=None):
+        instance = Vocabulary.objects.filter(english_word=english_word)
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
